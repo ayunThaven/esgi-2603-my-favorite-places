e2a8a8a8c6433dd6e4dcd1758b7f42d643e9df55
AyunThaven <brian.verchere@gmail.com>
2026-05-20 19:31:37 +0200

    Dockerisation du projet

--- server/src/datasource.ts
+++ server/src/datasource.ts
@@ -2,10 +2,11 @@ import { DataSource } from "typeorm";
 
 const datasource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  username: "postgres",
-  password: "supersecret",
-  database: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT || 5432),
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "postgres",
+  database: process.env.DB_NAME || "postgres",
   entities: [__dirname + "/entities/**/*.{js,ts}"],
   logging: true,
   synchronize: true,
